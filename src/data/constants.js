93d3298f53fefb0d5926b63890cdb64a02c9b960
aajliAhmed <Ahmed.conceptor@gmail.com>
2024-12-24 12:00:00 +0100

    add name degree

--- src/data/constants.js
+++ src/data/constants.js
@@ -316,7 +316,7 @@ export const education = [
     school: "EMSI",
     date: "Oct 2023 ",
     desc: "I'm currently a software engineering student at EMSI, specializing in Software Engineering and Intelligent Systems. My studies focus on advanced software development techniques, intelligent systems design, and practical applications of AI in software engineering.",
-    degree: "Software Engineering and Intelligent Systems",
+    degree: "Software Engineering and Networking",
   },
   {
     id: 1,
